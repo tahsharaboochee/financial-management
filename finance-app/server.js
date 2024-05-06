271ae183ceda8df503436176f600462e2eebd86d
Tahshara Boochee <boocheet@gmail.com>
2024-05-06 14:46:24 -0700

    update server file:

--- finance-app/server.js
+++ finance-app/server.js
@@ -1,14 +1,31 @@
+// Import necessary libraries
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Initialize dotenv to use environment variables
+dotenv.config();
 const app = express();
+
+const transactionsRouter = require('./routes/transactions');
+
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Middleware for parsing JSON bodies
 
-// Basic route
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+// Basic route i.e. root endpoint
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+// Use the transactions router for transaction-related routes
+app.use('/transactions', transactionsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
