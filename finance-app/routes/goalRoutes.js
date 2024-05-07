4d0687f7483c23990460c3b43077656d23307a9e
Tahshara Boochee <boocheet@gmail.com>
2024-05-07 16:37:24 -0700

    Goal Routes

---
+++ finance-app/routes/goalRoutes.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const Goal = require('../models/goal'); 
+const auth = require('../middleware/auth');
+const router = express.Router();
+
+// Create a new goal
+router.post('/', auth, async (req, res) => {
+    const goal = new Goal({
+        ...req.body,
+        user: req.user._id // Link the goal to the user
+    });
+    try {
+        await goal.save();
+        res.status(201).send(goal);
+    } catch (error) {
+        res.status(400).send({ error: 'Error creating goal' });
+    }
+});
+
+// Retrieve all goals for the logged-in user
+router.get('/', auth, async (req, res) => {
+    try {
+        const goals = await Goal.find({ user: req.user._id });
+        res.send(goals);
+    } catch (error) {
+        res.status(500).send({ error: 'Error fetching goals' });
+    }
+});
+
+// Update a specific goal
+router.patch('/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'targetAmount', 'currentAmount', 'deadline'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    try {
+        const goal = await Goal.findOne({ _id: req.params.id, user: req.user._id });
+
+        if (!goal) {
+            return res.status(404).send();
+        }
+
+        updates.forEach(update => goal[update] = req.body[update]);
+        await goal.save();
+        res.send(goal);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// Delete a specific goal
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const goal = await Goal.findOneAndDelete({ _id: req.params.id, user: req.user._id });
+
+        if (!goal) {
+            return res.status(404).send();
+        }
+
+        res.send(goal);
+    } catch (error) {
+        res.status(500).send({ error: 'Error deleting goal' });
+    }
+});
+
+module.exports = router;
