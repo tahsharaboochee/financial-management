ffdbc6bc8ade2bd160dbc4d786ec42cca9953d39
Tahshara Boochee <boocheet@gmail.com>
2024-05-07 15:59:43 -0700

    Additional routes for updating and deleting transactions

--- finance-app/routes/transactions.js
+++ finance-app/routes/transactions.js
@@ -27,4 +27,44 @@ router.post('/', auth, async (req, res) => {
     }
 });
 
+// Update a transaction
+router.patch('/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['type', 'amount', 'category', 'date'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    try {
+        const transaction = await Transaction.findOne({ _id: req.params.id, user: req.user._id });
+
+        if (!transaction) {
+            return res.status(404).send();
+        }
+
+        updates.forEach(update => transaction[update] = req.body[update]);
+        await transaction.save();
+        res.send(transaction);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// Delete a transaction
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const transaction = await Transaction.findOneAndDelete({ _id: req.params.id, user: req.user._id });
+
+        if (!transaction) {
+            res.status(404).send();
+        }
+
+        res.send(transaction);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
 module.exports = router;
